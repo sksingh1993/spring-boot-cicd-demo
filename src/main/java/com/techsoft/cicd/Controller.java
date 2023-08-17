d56096f27cfca6aced33fd168eddb50cc94929ad
Suraj Singh <kumarsurajsingh93@gmail.com>
2023-08-17 17:05:01 +0530

    Change done in controller

--- src/main/java/com/techsoft/cicd/Controller.java
+++ src/main/java/com/techsoft/cicd/Controller.java
@@ -3,11 +3,21 @@ package com.techsoft.cicd;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class Controller {
 
     @GetMapping("/message")
     public String getMessage(){
         return "Welcome to Tech soft !!";
+    }
+    @GetMapping("/number")
+    public Map<String,Integer> getNumber(){
+        Map<String, Integer> map = new HashMap<>();
+        map.put("One",1);
+        map.put("Ten",10);
+        return map;
     }
 }
